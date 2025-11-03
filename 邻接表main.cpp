84abe8aff3d41b1fc9a1a50998dead8dbe204e0c
yue74 <3336996173@qq.com>
2025-11-03 21:54:42 +0800

    Add files via upload

---
+++ 邻接表main.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+
+class gragh {
+private:
+	struct Edgenode {
+		int vertex;//»¡Î²ÐòºÅ
+		int weight;
+		Edgenode* next;
+	};
+	struct Vertexnode {
+		int vertex;//»¡Í·
+		Edgenode* firstedge;
+	};
+	int vertices;
+	Vertexnode* nodes;
+public:
+	gragh(int vertices);
+	~gragh();
+	void addedge(int u, int v, int w);
+	void printgragh();
+
+};
+gragh::gragh(int vertices) {
+	this->vertices = vertices;
+	nodes = new Vertexnode[vertices];
+	for (int i = 0; i < vertices; ++i) {
+		nodes[i].vertex = i;
+		nodes[i].firstedge = NULL;
+	}
+ }
+gragh::~gragh() {
+	for (int i = 0; i < vertices; ++i) {
+		Edgenode* curr = nodes[i].firstedge;
+		while (curr) {
+			Edgenode* temp = curr;
+			curr = curr->next;
+			delete temp;
+		}
+	}
+	delete[]nodes;
+}
+void gragh::addedge(int u, int v, int w) {//Í·²å·¨
+	Edgenode* newnode = new Edgenode;
+	newnode->vertex = v;
+	newnode->weight = w;
+	newnode->next = nodes[u].firstedge;
+	nodes[u].firstedge = newnode;
+}
+
+void gragh::printgragh() {
+	for (int i = 0; i < vertices; ++i) {
+		Edgenode* curr = nodes[i].firstedge;
+		cout << "vertex" << i << ":";
+		while (curr) {
+			cout << curr->vertex << "(" << curr->weight << ")";
+			curr = curr->next;
+		}
+		cout << endl;
+	}
+}
+int main() {
+	gragh g(5);
+	g.addedge(0,1,4);
+	g.addedge(0,2,2);
+	g.addedge(1,2,3);
+	g.addedge(2,3,4);
+	g.addedge(3,4,2);
+	g.printgragh();
+
+	return 0;
+}
